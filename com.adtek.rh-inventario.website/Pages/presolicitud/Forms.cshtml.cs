639bfb8c21182f3c9b593b733e637c4eddd97ec0
Hiram Chavez <virtual.liga@gmail.com>
2024-03-25 19:04:01 -0600

    Agregando primeras vistas con Partials

---
+++ com.adtek.rh-inventario.website/Pages/presolicitud/Forms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace com.adtek.rh_inventario.website.Pages.presolicitud
+{
+    public class FormsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
