7a70becea0ae75d0951a78f7b0fd7d1450fea101
Hiram Chavez <virtual.liga@gmail.com>
2024-04-04 12:36:37 -0600

    Feature: creando form y agregando js para activar/desactivar laboral

---
+++ com.adtek.rh-inventario.website/wwwroot/js/presolicitud/DatosRecomendacionBM.js
@@ -0,0 +1,15 @@
+﻿const tipoDeCarta = document.getElementById('tipoDeCarta');
+const inputPuesto = document.getElementById('puesto');
+const inputNombre = document.getElementById('empresa');
+
+tipoDeCarta.addEventListener('change', function () {
+    let value = tipoDeCarta.value;
+
+    if (value === 'Laboral') {
+        inputPuesto.disabled = false;
+        inputNombre.disabled = false;
+    } else {
+        inputPuesto.disabled = true;
+        inputNombre.disabled = true;
+    }
+});
