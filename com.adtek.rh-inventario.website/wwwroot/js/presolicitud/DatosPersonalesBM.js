4caf5eb8b98e1b426d49c8e2f2de56a9f937a5c8
Hiram Chavez <virtual.liga@gmail.com>
2024-04-04 09:57:28 -0600

    refactor: make form responsive. add js

---
+++ com.adtek.rh-inventario.website/wwwroot/js/presolicitud/DatosPersonalesBM.js
@@ -0,0 +1,20 @@
+﻿window.onload = function () {
+    datosPersonalesBM.inicializar();
+}
+
+const datosPersonalesBM = {
+
+    btnDPGuardar: document.getElementById('btnDPGuardar'),
+
+    guardar: function () {
+        alert("Guardando Datos Personales BM");
+    },
+
+    cargarEventos: function () {
+        datosPersonalesBM.btnDPGuardar.onclick = datosPersonalesBM.guardar;
+    },
+
+    inicializar: function () {
+        datosPersonalesBM.cargarEventos();
+    }
+};
