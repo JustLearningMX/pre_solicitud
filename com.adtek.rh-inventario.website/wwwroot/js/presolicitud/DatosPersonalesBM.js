ea7c321850524791fa6606324005b7863941a7ff
Hiram Chavez <virtual.liga@gmail.com>
2024-05-20 20:27:58 -0600

    Agregar Alert bootstrap

--- com.adtek.rh-inventario.website/wwwroot/js/presolicitud/DatosPersonalesBM.js
+++ com.adtek.rh-inventario.website/wwwroot/js/presolicitud/DatosPersonalesBM.js
@@ -15,6 +15,12 @@ const datosPersonalesBM = {
     txtDPCurp: document.getElementById('curp'),
     txtDPIne: document.getElementById('ine'),
 
+    alertContainer: document.querySelector('.alert'),
+    alertBtnClose: document.querySelector('#alert-close'),
+    alertTitle: document.querySelector('.alert-title'),
+    alertDetalle: document.querySelector('.alert-detalle'),
+    spinnerContainer: document.querySelector('.spinner-container'),
+
     guardar: function () {
 
         if (datosPersonalesBM.form.checkValidity()) {
@@ -38,13 +44,53 @@ const datosPersonalesBM = {
                 redirect: "follow"
             };
 
+            datosPersonalesBM.spinnerContainer.classList.add('spinner-grow');
+            datosPersonalesBM.btnDPGuardar.classList.add('disabled');
+            datosPersonalesBM.btnDPCancelar.classList.add('disabled');
+
             fetch("https://localhost:7078/api/DatosPersonales", requestOptions)
                 .then((response) => response.json())
                 .then((result) => {
+                    debugger
                     console.log(result);
-                    datosPersonalesBM.cancelar();
+                    datosPersonalesBM.alertContainer.classList.remove('alert-success', 'alert-warning', 'alert-danger');
+                    datosPersonalesBM.spinnerContainer.classList.remove('spinner-grow');
+
+                    if (Number(result.codigo) >= 200 && Number(result.codigo) <= 299) {
+                        // OK
+                        datosPersonalesBM.alertContainer.classList.add('alert-success');
+                        datosPersonalesBM.alertTitle.textContent = "Informacion guardada";
+                        datosPersonalesBM.alertDetalle.textContent = result.mensaje;
+                        datosPersonalesBM.cancelar();
+                    }
+                    else if (Number(result.codigo) >= 400 && Number(result.codigo) <= 499) {
+                        // Bad Request
+                        datosPersonalesBM.alertContainer.classList.add('alert-warning');
+                        datosPersonalesBM.alertTitle.textContent = result.mensaje;
+                        datosPersonalesBM.alertDetalle.innerHTML = result.detalles.join(' <br> ');
+                    }
+                    else {
+                        // Error
+                        datosPersonalesBM.alertContainer.classList.add('alert-danger');
+                        datosPersonalesBM.alertTitle.textContent = result.mensaje;
+                        datosPersonalesBM.alertDetalle.textContent = result.detalles.join(' <br> ');
+                    }
+
+                    datosPersonalesBM.alertContainer.classList.add('show');
+                    datosPersonalesBM.alertContainer.classList.remove('hide');
                 })
-                .catch((error) => console.error(error));
+                .catch((error) => {
+                    datosPersonalesBM.spinnerContainer.classList.remove('spinner-grow');
+                    datosPersonalesBM.alertContainer.classList.add('alert-danger');
+                    datosPersonalesBM.alertTitle.textContent = error.mensaje;
+                    datosPersonalesBM.alertDetalle.textContent = error.detalles.join(' <br> ');
+                    datosPersonalesBM.alertContainer.classList.add('show');
+                    datosPersonalesBM.alertContainer.classList.remove('hide');
+                })
+                .finally(() => {
+                    datosPersonalesBM.btnDPGuardar.classList.remove('disabled');
+                    datosPersonalesBM.btnDPCancelar.classList.remove('disabled');
+                });
         }
 
         datosPersonalesBM.form.classList.add('was-validated');
@@ -56,9 +102,15 @@ const datosPersonalesBM = {
         datosPersonalesBM.form.reset();
     },
 
+    cerrarAlerta: function () {
+        datosPersonalesBM.alertContainer.classList.remove('show');
+        datosPersonalesBM.alertContainer.classList.add('hide');
+    },
+
     cargarEventos: function () {
         datosPersonalesBM.btnDPGuardar.onclick = datosPersonalesBM.guardar;
         datosPersonalesBM.btnDPCancelar.onclick = datosPersonalesBM.cancelar;
+        datosPersonalesBM.alertBtnClose.onclick = datosPersonalesBM.cerrarAlerta;
     },
 
     inicializar: function () {
