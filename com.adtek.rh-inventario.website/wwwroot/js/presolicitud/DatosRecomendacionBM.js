1fcefe36f8cf7cdc0d228bd626124252bca59d75
Hiram Chavez <virtual.liga@gmail.com>
2024-05-21 15:10:29 -0600

    Agregar spinner y alert en Datos de Recomendacion

--- com.adtek.rh-inventario.website/wwwroot/js/presolicitud/DatosRecomendacionBM.js
+++ com.adtek.rh-inventario.website/wwwroot/js/presolicitud/DatosRecomendacionBM.js
@@ -3,7 +3,7 @@
 });
 
 const datosRecomendacionBM = {
-
+    
     form: document.getElementById('formDatosRecomendacion'),
     btnDRGuardar: document.getElementById('btnDRGuardar'),
     btnDRCancelar: document.getElementById('btnDRCancelar'),
@@ -14,8 +14,13 @@ const datosRecomendacionBM = {
     txtPuesto: document.getElementById('puesto'),
     txtEmpresa: document.getElementById('empresa'),
 
-    guardar: function () {
+    alertContainer: document.querySelector('.alert-dr'),
+    alertBtnClose: document.querySelector('#alert-close-dr'),
+    alertTitle: document.querySelector('.alert-title-dr'),
+    alertDetalle: document.querySelector('.alert-detalle-dr'),
+    spinnerContainer: document.querySelector('.spinner-container-dr'),
 
+    guardar: function () {
         if (datosRecomendacionBM.form.checkValidity()) {
             const myHeaders = new Headers();
             myHeaders.append("Content-Type", "application/json");
@@ -35,13 +40,52 @@ const datosRecomendacionBM = {
                 redirect: "follow"
             };
 
+            datosRecomendacionBM.spinnerContainer.classList.add('spinner-grow');
+            datosRecomendacionBM.btnDRGuardar.classList.add('disabled');
+            datosRecomendacionBM.btnDRCancelar.classList.add('disabled');
+
             fetch("https://localhost:7078/api/DatosRecomendacion", requestOptions)
                 .then((response) => response.json())
                 .then((result) => {
                     console.log(result);
-                    datosRecomendacionBM.cancelar();
+                    datosRecomendacionBM.alertContainer.classList.remove('alert-success', 'alert-warning', 'alert-danger');
+                    datosRecomendacionBM.spinnerContainer.classList.remove('spinner-grow');
+
+                    if (Number(result.codigo) >= 200 && Number(result.codigo) <= 299) {
+                        // OK
+                        datosRecomendacionBM.alertContainer.classList.add('alert-success');
+                        datosRecomendacionBM.alertTitle.textContent = "Informacion guardada";
+                        datosRecomendacionBM.alertDetalle.textContent = result.mensaje;
+                        datosRecomendacionBM.cancelar();
+                    }
+                    else if (Number(result.codigo) >= 400 && Number(result.codigo) <= 499) {
+                        // Bad Request
+                        datosRecomendacionBM.alertContainer.classList.add('alert-warning');
+                        datosRecomendacionBM.alertTitle.textContent = result.mensaje;
+                        datosRecomendacionBM.alertDetalle.innerHTML = result.detalles.join(' <br> ');
+                    }
+                    else {
+                        // Error
+                        datosRecomendacionBM.alertContainer.classList.add('alert-danger');
+                        datosRecomendacionBM.alertTitle.textContent = result.mensaje;
+                        datosRecomendacionBM.alertDetalle.textContent = result.detalles.join(' <br> ');
+                    }
+
+                    datosRecomendacionBM.alertContainer.classList.add('show');
+                    datosRecomendacionBM.alertContainer.classList.remove('hide');
                 })
-                .catch((error) => console.error(error));
+                .catch((error) => {
+                    datosRecomendacionBM.spinnerContainer.classList.remove('spinner-grow');
+                    datosRecomendacionBM.alertContainer.classList.add('alert-danger');
+                    datosRecomendacionBM.alertTitle.textContent = error.mensaje;
+                    datosRecomendacionBM.alertDetalle.textContent = error.detalles.join(' <br> ');
+                    datosRecomendacionBM.alertContainer.classList.add('show');
+                    datosRecomendacionBM.alertContainer.classList.remove('hide');
+                })
+                .finally(() => {
+                    datosRecomendacionBM.btnDRGuardar.classList.remove('disabled');
+                    datosRecomendacionBM.btnDRCancelar.classList.remove('disabled');
+                });
         }
 
         datosRecomendacionBM.form.classList.add('was-validated');
@@ -66,10 +110,17 @@ const datosRecomendacionBM = {
         datosRecomendacionBM.form.reset();
     },
 
+    cerrarAlertaDR: function () {
+        datosRecomendacionBM.alertContainer.classList.remove('show');
+        datosRecomendacionBM.alertContainer.classList.add('hide');
+    },
+
     cargarEventos: function () {
         datosRecomendacionBM.btnDRGuardar.onclick = datosRecomendacionBM.guardar;
         datosRecomendacionBM.txtTipoDeCarta.onchange = datosRecomendacionBM.habilitarCamposRecomendacionLaboral;
         datosRecomendacionBM.btnDRCancelar.onclick = datosRecomendacionBM.cancelar;
+        datosRecomendacionBM.alertBtnClose.onclick = datosRecomendacionBM.cerrarAlertaDR;
+
     },
 
     inicializar: function () {
